087e7528080b5c0003368903e379292a87816f89
torph <73564623+Torphedo@users.noreply.github.com>
2023-11-06 10:26:26 -0500

    Name constants, add comments, factor out MBC1 logic

--- src/memory_controllers.c
+++ src/memory_controllers.c
@@ -1,22 +1,70 @@
 #include <stdbool.h>
-#include <math.h>
 
+#include "machine.h"
 #include "memory_controllers.h"
 #include "bus.h"
 
-enum {
-    ENABLE_RAM = 0x1FFF,
-    ROM_BANK = 0x3FFF,
-    RAM_BANK = 0x5FFF,
-    MODE_FLAG = 0x7FFF
-}MBC1_fields;
-
 static struct {
-    uint8_t rom_bank;
-    uint8_t ram_bank;
-    bool mode;
-    bool ram_enabled;
+    uint8_t rom_bank: 5;
+    uint8_t ram_bank: 2;
+    bool mode: 1;
+    bool ram_enabled: 1;
 }controller_state;
+
+enum {
+    RANGE_MIN = 0x000
+};
+
+typedef enum {
+    MBC1_W_RANGE_ENABLE_RAM = 0x1FFF,
+    MBC1_W_RANGE_ROM_BANK = 0x3FFF,
+    MBC1_W_RANGE_RAM_BANK = 0x5FFF,
+    MBC1_W_RANGE_MODE_SEL = 0x7FFF,
+    MBC1_W_RANGE_EXT_RAM_LOW = 0xA000,
+    MBC1_W_RANGE_EXT_RAM_HIGH = 0xBFFF,
+}mbc1_w_range;
+
+typedef enum {
+    MBC1_R_RANGE_ROM_0 = 0x3FFF,
+    MBC1_R_RANGE_ROM_HIGH = 0x7FFF,
+}mbc1_r_range;
+
+typedef enum {
+    MBC3_W_RANGE_MIN = 0x0000,
+    MBC3_W_RANGE_
+}mbc3_w_range;
+
+bool in_range(uint16_t value, uint16_t low, uint16_t high) {
+    return (value >= low && value <= high);
+}
+
+bool range_mbc1_enable_ram(uint16_t addr) {
+    return in_range(addr, RANGE_MIN, MBC1_W_RANGE_ENABLE_RAM);
+}
+
+bool range_mbc1_rom_bank(uint16_t addr) {
+    return in_range(addr, MBC1_W_RANGE_ENABLE_RAM, MBC1_W_RANGE_ROM_BANK);
+}
+
+bool range_mbc1_ram_bank(uint16_t addr) {
+    return in_range(addr, MBC1_W_RANGE_ROM_BANK, MBC1_W_RANGE_RAM_BANK);
+}
+
+bool range_mbc1_mode_sel(uint16_t addr) {
+    return in_range(addr, MBC1_W_RANGE_RAM_BANK, MBC1_W_RANGE_MODE_SEL);
+}
+
+bool range_mbc1_ext_ram(uint16_t addr) {
+    return in_range(addr, MBC1_W_RANGE_EXT_RAM_LOW, MBC1_W_RANGE_EXT_RAM_HIGH);
+}
+
+bool range_mbc1_rom_0(uint16_t addr) {
+    return in_range(addr, RANGE_MIN, MBC1_R_RANGE_ROM_0);
+}
+
+bool range_mbc1_rom_high(uint16_t addr) {
+    return in_range(addr, MBC1_R_RANGE_ROM_0, MBC1_R_RANGE_ROM_HIGH);
+}
 
 // Size of a 16KiB ROM bank.
 #define ROM_BANK_SIZE 0x4000
@@ -57,10 +105,10 @@ uint8_t zero_bank_number(uint8_t rom_bank_count, uint8_t ram_bank_count) {
     if (rom_bank_count <= 32) {
         return 0;
     }
-    if (rom_bank_count <= 64) {
+    else if (rom_bank_count <= 64) {
         return ram_bank_count << 4;
     }
-    if (rom_bank_count <= 128) {
+    else if (rom_bank_count <= 128) {
         return ram_bank_count << 5;
     }
 
@@ -77,11 +125,11 @@ uint8_t high_bank_number(uint8_t rom_bank_count, uint8_t ram_bank_count) {
     if (rom_bank_count <= 32) {
         return base_bank_num;
     }
-    if (rom_bank_count <= 64) {
+    else if (rom_bank_count <= 64) {
         // AND value with the lowest bit of the ram bank count
         return base_bank_num & ((ram_bank_count & 0b00000001) << 5);
     }
-    if (rom_bank_count <= 128) {
+    else if (rom_bank_count <= 128) {
         // AND value with the lowest 2 bits of the ram bank count
         return base_bank_num & ((ram_bank_count & 0b00000011) << 5);
     }
@@ -96,86 +144,95 @@ bool init_memory_controller(cart_header* cart) {
     return true;
 }
 
-uint8_t* controller_read(uint16_t address, const machine_state* machine) {
-    switch (machine->memory_controller) {
-        case MBC1:
-            if (address <= 0x3FFF) {
-                if (controller_state.mode == 1) {
-                    uint8_t zero_bank_num = zero_bank_number(machine->rom_bank_count, machine->ram_bank_count);
-                    return &machine->cartridge_rom[(ROM_BANK_SIZE * zero_bank_num) + address];
-                }
-                else {
-                    break;
-                }
-            }
-            else if (address <= 0x7FFF) {
-                // Offset within the bank that the address is pointing to
-                uint16_t relative_address = address - ROM_BANK_SIZE;
-                uint8_t high_bank_num = high_bank_number(machine->rom_bank_count, machine->ram_bank_count) + 1;
-                return &machine->cartridge_rom[(ROM_BANK_SIZE * high_bank_num) + relative_address];
-            }
-            else if (bus_address_in_external_ram(address)) {
-                if (!controller_state.ram_enabled) {
-                    return (uint8_t*) &invalid_data;
-                }
-
-                uint16_t relative_address = address - 0xA000;
-                if (controller_state.mode == 1) {
-                    return &machine->external_ram[(RAM_BANK_SIZE * controller_state.ram_bank) + relative_address];
-                }
-                else {
-                    return &machine->external_ram[relative_address];
-                }
-            }
-        default:
-            break;
+uint8_t* mbc1_read(uint16_t addr, const machine_state* machine) {
+    // Read from ROM bank 0
+    if (range_mbc1_rom_0(addr)) {
+        if (controller_state.mode != 0) {
+            uint8_t zero_bank_num = zero_bank_number(machine->rom_bank_count, machine->ram_bank_count);
+            addr += (ROM_BANK_SIZE * zero_bank_num);
+        }
+        return &machine->cartridge_rom[addr];
     }
-    return &machine->console_memory[address];
+    else if (range_mbc1_rom_high(addr)) {
+        // Offset within the bank that the addr is pointing to
+        uint8_t high_bank_num = high_bank_number(machine->rom_bank_count, machine->ram_bank_count) + 1;
+        uint16_t offset = ROM_BANK_SIZE * high_bank_num + (addr - ROM_BANK_SIZE);
+        return &machine->cartridge_rom[offset];
+    }
+    else if (range_mbc1_ext_ram(addr)) {
+        // Trying to read from disabled RAM always returns 0xFF values.
+        if (!controller_state.ram_enabled) {
+            return (uint8_t*) &invalid_data;
+        }
+    
+        uint16_t offset = addr - MBC1_W_RANGE_EXT_RAM_LOW;
+        if (controller_state.mode == 1) {
+            offset += RAM_BANK_SIZE * controller_state.ram_bank;
+        }
+        return &machine->external_ram[offset];
+    }
+    return (uint8_t*) &invalid_data;
 }
 
-void controller_write_8_bit(uint16_t address, uint8_t value, const machine_state* machine) {
+uint8_t* controller_read(uint16_t addr, const machine_state* machine) {
+    switch (machine->memory_controller) {
+    case MBC1:
+        return mbc1_read(addr, machine);
+        break;
+    default:
+        break;
+    }
+    return &machine->console_memory[addr];
+}
+
+void write_mbc1_8(uint16_t addr, uint8_t value, const machine_state* machine) {
+    if (range_mbc1_enable_ram(addr)) {
+        controller_state.ram_enabled = ((value | 0xF) == 0xA);
+    }
+    else if (range_mbc1_rom_bank(addr)) {
+        // Exclude 3 bits because this is a 5-bit field.
+        // Stops us from writing a bank number larger than the number of
+        // available banks.
+        uint8_t bitmask = (machine->rom_bank_count - 1) & 0b00011111;
+    
+        controller_state.rom_bank = value & bitmask;
+        if (controller_state.rom_bank == 0) {
+            controller_state.rom_bank = 1;
+        }
+    }
+    else if (range_mbc1_ram_bank(addr)) {
+        // Get lowest 2 bits of value
+        controller_state.ram_bank = value & 0b00000011;
+    }
+    else if (range_mbc1_mode_sel(addr)) {
+        controller_state.mode = value & 0b00000001;
+    }
+    else if (range_mbc1_ext_ram(addr)) {
+        // Early exit if RAM is locked.
+        if (!controller_state.ram_enabled) {
+            return;
+        }
+
+        // Offset from RAM start
+        uint32_t relative_address = addr - MBC1_W_RANGE_EXT_RAM_LOW;
+
+        if (machine->ram_bank_count <= 1) {
+            // The mod operator here stops an out of bounds write
+            relative_address %= RAM_BANK_SIZE;
+        }
+        // If mode flag is 0, we just use the offset.
+        if (controller_state.mode == 1) {
+            relative_address += (RAM_BANK_SIZE * controller_state.ram_bank);
+        }
+        machine->external_ram[relative_address] = value;
+    }
+}
+
+void controller_write_8_bit(uint16_t addr, uint8_t value, const machine_state* machine) {
     switch (machine->memory_controller) {
         case MBC1:
-            if (address <= ENABLE_RAM) {
-                controller_state.ram_enabled = (value == 0xA);
-            }
-            else if (address <= ROM_BANK) {
-                // Limit the bitmask so that it's impossible to write a number to controller_state.rom_bank
-                // larger than the number of available banks.
-                uint8_t bitmask = (machine->rom_bank_count - 1) & 0b00011111; // Exclude 3 bits because this is a 5-bit field
-
-                controller_state.rom_bank = value & bitmask;
-                if (controller_state.rom_bank == 0) {
-                    controller_state.rom_bank = 1;
-                }
-            }
-            else if (address <= RAM_BANK) {
-                controller_state.ram_bank = value & 0b00000011; // Get lowest 2 bits of value
-            }
-            else if (address <= MODE_FLAG) {
-                controller_state.mode = value & 0b00000001;
-            }
-
-            if (bus_address_in_external_ram(address)) {
-                if (!controller_state.ram_enabled) {
-                    return;
-                }
-                else {
-                    if (machine->ram_bank_count <= 1) {
-                        // The mod operator here stops an out of bounds write
-                        uint16_t relative_address = (address - 0xA000) % RAM_BANK_SIZE;
-                        machine->external_ram[relative_address] = value;
-                    }
-                    if (controller_state.mode == 0) {
-                        uint16_t relative_address = address - 0xA000;
-                        machine->external_ram[relative_address] = value;
-                    }
-                    else {
-                        uint32_t relative_address = (RAM_BANK_SIZE * controller_state.ram_bank) + address - 0xA000;
-                        machine->external_ram[relative_address] = value;
-                    }
-                }
-            }
+            write_mbc1_8(addr, value, machine);
+            break;
         default:
             break;
     }
